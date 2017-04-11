6fa91b7b4bcf6b2a15c0dda9b262ca5f7d5c933f
Lucille Blumire <llblumire@gmail.com>
2017-04-11 19:27:33 +0100

    add exit code handling

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -11,12 +11,14 @@ using namespace demon;
 int main() {
     // Initialize RNG
     srand(time(NULL));
+    int exit_code = 0;
 
     GameState game = GameState();
 
     result::Result<Ending::Enum, Error::Enum>* game_result = game.run();
     {
         Ending::Enum* ending;
+        Error::Enum* error;
         if (game_result->let_ok(ending)) {
             switch (*ending) {
                 case Ending::Win:
@@ -26,9 +28,19 @@ int main() {
                     std::cout << "Try Again Next Time!";
                     break;
             }
+        } else if (game_result->let_err(error)) {
+            switch (*error) {
+                case Error::Unknown:
+                    std::cout << "An Unknown Error Occured";
+                    break;
+            }
+
+            exit_code = 1;
         }
     }
     delete game_result;
 
     std::cout << std::endl;
+
+    return exit_code;
 }
